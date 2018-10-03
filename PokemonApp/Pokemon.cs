8aa04414f4255072c9b676af0214f58d35c7c49c
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-10-03 00:47:51 -0700

    Add exp and levels properties to Pokemon

--- PokemonApp/Pokemon.cs
+++ PokemonApp/Pokemon.cs
@@ -10,15 +10,20 @@ namespace PokemonApp
         public string Name { get; set; }
         public int Hp { get; set; }
         public int BaseAttack { get; set; }
-
-        public Pokemon(string name, int hp, int baseAttack)
+        public int Level { get; set;}
+        public int Exp { get; set; }
+        public int ExpToLevel { get; set; }
+        public int ExpReleased { get; set; }
+        // Add Attack Skills array
+        public Pokemon(string name, int hp, int baseAttack, int level)
         {
             this.Name = name;
             this.Hp = hp;
             this.BaseAttack = baseAttack;
-
-
-
+            this.Level = level;
+            this.Exp = 0;
+            this.ExpToLevel = (int)Math.Round(Math.Pow(this.Level/.22, 2), 0);
+            this.ExpReleased = (int)Math.Round(this.ExpToLevel / Math.Pow((1.5*this.Level), .75), 0) + 2;
         }
         public int Attack(ref Pokemon opponent)
         {
@@ -30,5 +35,18 @@ namespace PokemonApp
 
             return opponent.Hp -= attackRange;
         }
+        public void GainExp(ref Pokemon opponent)
+        {
+            this.Exp += opponent.ExpReleased;
+        }
+        public void LevelUp()
+        {
+            if (this.Exp >= this.ExpToLevel)
+            {
+                this.Level++;
+                this.Exp -= this.ExpToLevel;
+            }
+        }
+
     }
 }
