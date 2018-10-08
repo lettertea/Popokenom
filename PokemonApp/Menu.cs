79f9a2ea1a0f0150fd10c568500d9135f15fdfad
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-10-07 23:16:47 -0700

    Start Menu

---
+++ PokemonApp/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonApp
+{
+    class Menu
+    {
+        public void PromptChoices()
+        {
+            Console.WriteLine("1. Fight wild Pokemons.");
+            if (int.TryParse(Console.ReadLine(), out int menuChoice))
+            {
+                switch (menuChoice)
+                { case 1:
+                        Console.WriteLine("What level? [1-100]");
+                        if (int.TryParse(Console.ReadLine(), out int opponentLevel))
+                        {
+                            Pokemon opponent = new Pokemon(Pokemons.GetRandomPokemon(), opponentLevel);
+                        }
+                            break;
+
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+    }
+}
