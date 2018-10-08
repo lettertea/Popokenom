25cc8f120621ee0a68cb3a2a2b0578f0b0300e91
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-10-07 23:16:08 -0700

    Apply expression bodied properties and RandomRange

--- PokemonApp/Pokemon.cs
+++ PokemonApp/Pokemon.cs
@@ -6,45 +6,48 @@ namespace PokemonApp
 {
     class Pokemon
     {
-
+        Random rand = new Random();
+        public float Rarity => rand.Next(70,131) / 100;
         public string Name { get; set; }
         public int Hp { get; set; }
         public int BaseAttack { get; set; }
-        public int Level { get; set;}
+        public int Level { get; set; }
         public int Exp { get; set; }
-        public int ExpToLevel { get; set; }
-        public int ExpReleased { get; set; }
-        // Add Attack Skills array
-        public Pokemon(string name, int hp, int baseAttack, int level)
+        public int ExpToLevel => (int)Math.Round(Math.Pow(this.Level / .17, 2), 0);
+        public int ExpReleased => (int)Math.Round((this.ExpToLevel / (.1 * Math.Pow(this.Level, 1.3) + 1) * (1 + this.Rarity)), 0) ; // expression bodied property
+        public int MaxHp => RandomRange.GetRandomRange((int)Math.Round((this.Level *100) * Math.Pow(.2*this.Level, 1.17) , 0), this.Rarity);
+        // Todo: add Type
+
+
+        public Pokemon(string name, int level)
         {
             this.Name = name;
-            this.Hp = hp;
-            this.BaseAttack = baseAttack;
             this.Level = level;
+            this.Hp = this.MaxHp;
             this.Exp = 0;
-            this.ExpToLevel = (int)Math.Round(Math.Pow(this.Level/.22, 2), 0);
-            this.ExpReleased = (int)Math.Round(this.ExpToLevel / Math.Pow((1.5*this.Level), .75), 0) + 2;
-        }
-        public int Attack(ref Pokemon opponent)
-        {
-            int lowestAttack = (int)Math.Round(this.BaseAttack - this.BaseAttack * .1, 0);
-            int highestAttack = (int)Math.Round(this.BaseAttack + this.BaseAttack * .1, 0);
 
-            Random rand = new Random();
-            int attackRange = rand.Next(lowestAttack, highestAttack + 1);
+        }
 
-            return opponent.Hp -= attackRange;
-        }
-        public void GainExp(ref Pokemon opponent)
+
+        public int Attack(ref Pokemon opponent) => opponent.Hp -= this.BaseAttack;
+
+
+        public int GainExp(Pokemon opponent)
         {
-            this.Exp += opponent.ExpReleased;
+            int expReleasedRange = RandomRange.GetRandomRange(opponent.ExpReleased, 0.1f);
+            this.Exp += expReleasedRange;
+            this.LevelUp();
+            opponent.Level = this.Level;
+            return expReleasedRange;
         }
+
         public void LevelUp()
         {
             while (this.Exp >= this.ExpToLevel)
             {
-                this.Level++;
                 this.Exp -= this.ExpToLevel;
+                this.Level++;
+                Console.WriteLine($"You are Level {this.Level}");
             }
         }
 
---
+++ PokemonApp/RandomRange.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonApp
+{
+    class RandomRange
+    {
+        public static int GetRandomRange(int baseInt, float variation)
+        {
+            int lowestInt = (int)Math.Round(baseInt* (1 - variation), 0);
+            int highestInt = (int)Math.Round(baseInt * (1 + variation), 0);
+
+            Random rand = new Random();
+            int intRange = rand.Next(lowestInt, highestInt + 1);
+            return intRange;
+        }
+    }
+}
