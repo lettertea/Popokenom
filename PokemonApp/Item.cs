93b0155e7fbc645257e57f1678a846c33b22b3d1
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-10-24 22:05:38 -0700

    Create Item class

---
+++ PokemonApp/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonApp
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public Item(string name, int price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+}
