02e61535c00fdc336dc1a9a26b8a19155f044bc4
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-10-24 22:31:06 -0700

    Create Pokeball class

---
+++ PokemonApp/Pokeball.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonApp
+{
+    class Pokeball : Item
+    {
+        public float Effectiveness { get; set; }
+        public Pokeball(string name, int price, float effectiveness) : base(name, price)
+        {
+            this.Effectiveness = effectiveness;
+        }
+
+    }
+}
