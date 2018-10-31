b460565195dfc9093ddf24059833b4abe64a4be2
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-10-31 14:33:46 -0700

    Add cancel option for menu input

--- PokemonApp/Menu.cs
+++ PokemonApp/Menu.cs
@@ -6,22 +6,31 @@ namespace PokemonApp
 {
     class Menu
     {
-        public static int GetUserInputIndex(string[] options)
+        public static int GetUserInputIndex(List<string> options, bool cancel)
         {
             int userInputIndex;
             do
             {
-                for (int i = 0; i < options.Length; i++)
+                int i;
+                for (i = 0; i < options.Count; i++)
                 {
                     string option = options[i];
                     Console.WriteLine($"{i + 1}. {option}");
                 }
 
-                if (int.TryParse(Console.ReadLine(), out userInputIndex))
+                int cancelIndex = -1;
+                if (cancel)
+                { 
+                cancelIndex = i;
+                options.Add($"{cancelIndex + 1}. Cancel");
+                }
+
+                if (int.TryParse(Console.ReadLine(), out int userInput))
                 {
-                    if (userInputIndex >= 1 && userInputIndex <= options.Length)
+                    userInputIndex = userInput - 1;
+                    if (userInputIndex >= 0 && userInput < options.Count)
                     {
-                        return userInputIndex-1;
+                        return userInputIndex == cancelIndex ? -1 : userInputIndex;
                     }
                     Console.WriteLine("Try again");
                 }
@@ -39,6 +48,7 @@ namespace PokemonApp
                     if (opponentLevel >= 1 && opponentLevel <= 100)
                     {
                         Pokemon opponent = new Pokemon(Pokemons.GetRandomPokemon(), opponentLevel);
+                        Console.WriteLine($"A wild {opponent.Name} appeared.");
                         Battle.Fight(ref userPokemons, ref opponent);
                         break;
                     }
