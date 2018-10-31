b79bd4d4566281257f3c1878b2025539b74ce27d
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-10-31 14:35:20 -0700

    Add Item base class

--- PokemonApp/Item.cs
+++ PokemonApp/Item.cs
@@ -4,14 +4,18 @@ using System.Text;
 
 namespace PokemonApp
 {
-    class Item
+    abstract class Item
     {
         public string Name { get; set; }
         public int Price { get; set; }
+        public abstract string PokemonAffected { get; }
         public Item(string name, int price)
         {
             this.Name = name;
             this.Price = price;
         }
+
+        public abstract void Use(ref Pokemon pokemon, ref PokemonTrainer userTrainer);
+
     }
 }
