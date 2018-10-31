99b144d6f2ec7d772cb80b4c72c676c0058c5ca3
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-10-31 14:36:19 -0700

    Add Pokeball and PokeballStore

--- PokemonApp/Pokeball.cs
+++ PokemonApp/Pokeball.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace PokemonApp
 {
     class Pokeball : Item
     {
-        public float Effectiveness { get; set; }
-        public Pokeball(string name, int price, float effectiveness) : base(name, price)
+        public double Effectiveness { get; set; }
+        public override string PokemonAffected => "opponent";
+
+        public Pokeball(string name, int price, double effectiveness) : base(name, price)
         {
             this.Effectiveness = effectiveness;
+        }
+
+        public override void Use(ref Pokemon opponent, ref PokemonTrainer userTrainer)
+        {
+            userTrainer.Items.Remove(this);
+            double captureProbability = (double)(this.Effectiveness * opponent.CaptureProbability);
+            Random rand = new Random();
+            if (captureProbability * this.Effectiveness >= rand.NextDouble()) {
+                userTrainer.CaptivePokemons.Add(opponent);
+                Console.WriteLine($"{opponent.Name} has been caught!");
+                Console.Write("While trying to break out, ");
+                opponent.Hp = 0;
+                return;
+            }
+            Console.WriteLine($"{opponent.Name} broke out!");
         }
 
     }
---
+++ PokemonApp/PokeballStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonApp
+{
+    class PokeballStore
+    {
+        public static Pokeball GetPokeball() => new Pokeball("Pokeball (It's a ball of poke)", "opponent", 50, 1.0);
+        public static Pokeball GetGreatball() => new Pokeball("Great Ball (It's pretty great)", "opponent", 150, 1.5);
+        public static Pokeball GetUltraball() => new Pokeball("Ultra Ball (I have no puns for this)", "opponent", 500, 2.0);
+    }
+}
