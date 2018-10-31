ada85f572f859c82bcf91b776dec0ce5d93359c7
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-10-31 14:37:53 -0700

    Add CaptureProbability property

--- PokemonApp/Pokemon.cs
+++ PokemonApp/Pokemon.cs
@@ -26,8 +26,10 @@ namespace PokemonApp
         public int Exp { get; set; }
         public int ExpToLevel => (int)Math.Round(Math.Pow(this.Level / .17, 2), 0);
         public int ExpReleased => (int)Math.Round((this.ExpToLevel / (.1 * Math.Pow(this.Level, 1.3) + 1) * (this.Rarity)), 0); // expression bodied property
-        public int MaxHp => (int)Math.Round(((this.Level * 10) * Math.Pow(.2 * this.Level+1, 1.17) * this.Rarity), 0);
+        public int MaxHp => (int)Math.Round(((this.Level * 10) * Math.Pow(.2 * this.Level + 1, 1.17) * this.Rarity), 0);
         public int BaseAttack => (int)Math.Round(this.MaxHp / 3.0, 0);
+        public double CaptureProbability => (double)this.Hp/this.MaxHp; 
+
 
 
         Random rand = new Random();
@@ -38,7 +40,6 @@ namespace PokemonApp
             this.Level = level;
             this.Hp = this.MaxHp;
             this.Exp = 0;
-
         }
 
 
