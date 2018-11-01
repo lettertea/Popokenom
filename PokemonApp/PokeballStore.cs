6b1b2c2c1f0834c35135d569dfe0eeb401fe0718
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-10-31 23:30:03 -0700

    Remove "opponent" string in Pokeball instantiations

--- PokemonApp/PokeballStore.cs
+++ PokemonApp/PokeballStore.cs
@@ -6,8 +6,8 @@ namespace PokemonApp
 {
     class PokeballStore
     {
-        public static Pokeball GetPokeball() => new Pokeball("Pokeball (It's a ball of poke)", "opponent", 50, 1.0);
-        public static Pokeball GetGreatball() => new Pokeball("Great Ball (It's pretty great)", "opponent", 150, 1.5);
-        public static Pokeball GetUltraball() => new Pokeball("Ultra Ball (I have no puns for this)", "opponent", 500, 2.0);
+        public static Pokeball GetPokeball() => new Pokeball("Pokeball (It's a ball of poke)", 50, 1.0);
+        public static Pokeball GetGreatball() => new Pokeball("Great Ball (It's pretty great)", 150, 1.5);
+        public static Pokeball GetUltraball() => new Pokeball("Ultra Ball (I have no puns for this)", 500, 2.0);
     }
 }
