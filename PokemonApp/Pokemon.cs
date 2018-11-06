3ff82e123dc958dc779b463d062d482ebedb9b10
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-11-06 12:27:03 -0800

    Remove rounding and just floor to integer

--- PokemonApp/Pokemon.cs
+++ PokemonApp/Pokemon.cs
@@ -21,13 +21,11 @@ namespace PokemonApp
             }
         }
         public int Exp { get; set; }
-        public int ExpToLevel => (int)Math.Round(Math.Pow(this.Level / .17, 2), 0);
-        public int ExpReleased => (int)Math.Round((this.ExpToLevel / (.1 * Math.Pow(this.Level, 1.3) + 1) * (this.Rarity)), 0);
-        public int MaxHp => (int)Math.Round(((this.Level * 10) * Math.Pow(.2 * this.Level + 1, 1.17) * this.Rarity), 0);
-        public int BaseAttack => (int)Math.Round(this.MaxHp / 3.0, 0);
+        public int ExpToLevel => (int)Math.Pow(this.Level / .17, 2);
+        public int ExpReleased => (int)(this.ExpToLevel / (.1 * Math.Pow(this.Level, 1.3) + 1) * (this.Rarity));
+        public int MaxHp => (int)((this.Level * 10) * Math.Pow(.2 * this.Level + 1, 1.17) * this.Rarity);
+        public int BaseAttack => this.MaxHp / 3;
         public double CaptureProbability => (double)this.Hp/this.MaxHp; 
-
-
 
         Random rand = new Random();
         public Pokemon(string name, int level)
