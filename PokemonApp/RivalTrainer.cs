f042919066d2e2cb239e34364adf6c8a9440feaa
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-11-12 19:59:56 -0800

    Create Level property and update to its usage

--- PokemonApp/RivalTrainer.cs
+++ PokemonApp/RivalTrainer.cs
@@ -6,36 +6,38 @@ namespace PokemonApp
 {
     class RivalTrainer : PokemonTrainer
     {
-        public RivalTrainer(string name) : base(name)
+        public int Level { get; set; }
+        public RivalTrainer(string name, int level) : base(name)
         {
             this.Name = name;
+            this.Level = level;
         }
 
-        public static void SetRivalPokemons(string userStarterPokemon, PokemonTrainer rival, int index)
+        public void SetPokemons(string userStarterPokemon)
         {
-            int pokemonLevel = (index + 1) * 10;
+            int pokemonLevel = (this.Level + 1) * 10;
 
             // Set starter pokemon
-            if (userStarterPokemon == "Bulbasaur") { rival.CaptivePokemons.Add(new Pokemon("Charmander", pokemonLevel)); }
-            else if (userStarterPokemon == "Charmander") { rival.CaptivePokemons.Add(new Pokemon("Squirtle", pokemonLevel)); }
-            else { rival.CaptivePokemons.Add(new Pokemon("Bulbasaur", pokemonLevel)); }
+            if (userStarterPokemon == "Bulbasaur") { this.CaptivePokemons.Add(new Pokemon("Charmander", pokemonLevel)); }
+            else if (userStarterPokemon == "Charmander") { this.CaptivePokemons.Add(new Pokemon("Squirtle", pokemonLevel)); }
+            else { this.CaptivePokemons.Add(new Pokemon("Bulbasaur", pokemonLevel)); }
 
 
-            if (index == 9)
+            if (this.Level == 9)
             {
-                for (int i = 0; i < 5; i++) { rival.CaptivePokemons.Add(new Pokemon("Magikarp", pokemonLevel)); }
+                for (int i = 0; i < 5; i++) { this.CaptivePokemons.Add(new Pokemon("Magikarp", pokemonLevel)); }
             }
-            else if (index > 3)
+            else if (this.Level > 3)
             {
-                for (int i = 0; i < 2; i++) { rival.CaptivePokemons.Add(new Pokemon("Magikarp", pokemonLevel)); }
+                for (int i = 0; i < 2; i++) { this.CaptivePokemons.Add(new Pokemon("Magikarp", pokemonLevel)); }
                 for (int i = 0; i < 3; i++)
                 {
-                    rival.CaptivePokemons.Add(new Pokemon(GetNonMagikarpPokemon(), pokemonLevel));
+                    this.CaptivePokemons.Add(new Pokemon(GetNonMagikarpPokemon(), pokemonLevel));
                 }
             }
-            else if (index > 0)
+            else if (this.Level > 0)
             {
-                for (int i = 0; i < 2; i++) { rival.CaptivePokemons.Add(new Pokemon(GetNonMagikarpPokemon(), pokemonLevel)); }
+                for (int i = 0; i < 2; i++) { this.CaptivePokemons.Add(new Pokemon(GetNonMagikarpPokemon(), pokemonLevel)); }
             }
 
         }
