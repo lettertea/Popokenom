7cd18991eb86f3df55defd18a1f96915e9958f5f
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-11-13 09:09:05 -0800

    Combine all stores to one item store file

---
+++ PokemonApp/ItemStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonApp
+{
+    class ItemStore
+    {
+        private List<Item> _items = new List<Item> {
+   new Pokeball("Pokeball (It's a ball of poke)", 50, 1.0),
+   new Pokeball("Great Ball (It's pretty great)", 150, 1.5),
+   new Pokeball("Ultra Ball (I have no puns for this)", 500, 2.0),
+   new Pokeball("Pokeball (It's a ball of poke)", 50, 1.0),
+   new Pokeball("Great Ball (It's pretty great)", 150, 1.5),
+   new Pokeball("Ultra Ball (I have no puns for this)", 500, 2.0)
+  };
+    }
+}
+}
